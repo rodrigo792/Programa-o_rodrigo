f85cc609256254dbcc2e6702e0efb079a062d7cd
rodrigo792 <rodrigoalvarenga790@gmail.com>
2026-02-27 16:38:19 -0300

    Add files via upload

---
+++ Complemento/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Complemento
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int A, B, C;
+
+            Console.WriteLine("Digite o valor de A:");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de B:");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de C:");
+            C = Convert.ToInt32(Console.ReadLine());
+
+            if (A > B && A > C)
+            {
+                Console.WriteLine("O maior valor é: " + A);
+            }
+            else if (B > A && B > C)
+            {
+                Console.WriteLine("O maior valor é: " + B);
+            }
+            else
+            {
+                Console.WriteLine("O maior valor é: " + C);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
